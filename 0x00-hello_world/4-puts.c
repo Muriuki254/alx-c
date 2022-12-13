b18c79dfd38a9166d37da508c4d311b30b3f787d
Ian-Muriuki <ianringera@gmail.com>
2022-12-12 22:39:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
-/**
- *
- * main - main block
- *
- * Return: always 0 (Success)
- */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingualpuzzle");
 	return (0);
-
 }
